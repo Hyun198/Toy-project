5cfe639a5caf42e68bc00fb247672656d92a93e8
Hyun <hwn123h@naver.com>
2023-12-07 12:36:30 +0900

    테스트 성공

--- test.js
+++ test.js
@@ -6,27 +6,48 @@ const weatherDetails = document.querySelector('.weather-details');
 
 search.addEventListener('click', () => {
     const APIKey = '2d9656e12a5cfa0fd6b7cbebd84d6e23';
-    const city = document.querySelector('.serach-box input').value;
-    if (city == '') {
+    const city = document.querySelector('.search-box input').value;
+    if (city == '')
         return;
-    }
+
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
         .then(response => response.json()).then(json => {
 
-            const image = document.querySelector('weather-box i');
-            const temperature = document.querySelector('weather-box .temperature');
-            const description = document.querySelector('weather-box .description');
-            const humidity = document.querySelector('weather-details .humidity span');
-            const wind = document.querySelector('weather-details .wind span');
-
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
 
             switch (json.weather[0].main) {
                 case 'Clear':
-                    image.src = '';
+                    image.src = '../img/sunny.png';
+                    break;
+                case 'Rain':
+                    image.src = '../img/rainy.png';
+                    break;
+                case 'Clouds':
+                    image.src = '../img/cloudy.png';
+                    break;
 
+                case 'Snow':
+                    image.src = '../img/snow.png';
+                    break;
+
+                case 'Moist':
+                    image.src = '../img/google.png';
+                    break;
+                case 'Haze':
+                    image.src = '../img/google.png';
                     break;
                 default:
+                    image.src = '../img/sunny.png';
                     break;
             }
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>&#176;</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
         })
 })
