22fc2926cc1efb597e053feefccbbb625ea6ad25
Hyun <hwn123h@naver.com>
2024-02-18 22:43:26 +0900

    뉴스

--- readme.md
+++ readme.md
@@ -1,7 +1,11 @@
 ### javascript 연습
 
+    async/ await api호출
+
 ## Up and Down 게임
 
 ## To-do List
 
 ## CGV 출근 시간 계산
+
+## 뉴스 api
---
+++ 뉴스/index.js
@@ -0,0 +1,15 @@
+const api_key = `cbf51c2e05b8463fa355ed9a4dbe97a8`
+let news = []
+const LatestNews = async () => {
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${api_key}`);
+    const response = await fetch(url)
+    const data = response.json();
+    news = data.articles
+    console.log(news);
+
+}
+
+LatestNews();
+
+
+
