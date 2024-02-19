e542a770a79e108d9002aaab84b340f578da0b61
Hyun <hwn123h@naver.com>
2024-02-19 12:43:18 +0900

    리팩토링

--- 뉴스/index.js
+++ 뉴스/index.js
@@ -1,13 +1,20 @@
 const api_key = `cbf51c2e05b8463fa355ed9a4dbe97a8`
 let newsList = []
-const LatestNews = async () => {
-    url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${api_key}`);
-    const response = await fetch(url)
+const menus = document.querySelectorAll(".menu-bar li");
+menus.forEach((menu) => menu.addEventListener("click", (e) => getNewsByCategory(e)))
+
+let url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${api_key}`);
+
+const getArticle = async () => {
+    const response = await fetch(url);
     const data = await response.json();
     newsList = data.articles;
     render();
-    console.log(newsList);
+}
 
+const LatestNews = async () => {
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${api_key}`);
+    getArticle();
 }
 
 const render = () => {
@@ -36,6 +43,18 @@ const render = () => {
     document.getElementById('news-board').innerHTML = newsHTML
 }
 
+const getNewsByCategory = async (e) => {
+    const category = e.target.textContent.toLowerCase();
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${api_key}`)
+    getArticle();
+}
+
+
+const getNewsbyKeyword = async () => {
+    const keyword = document.getElementById("search-input").value;
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${api_key}`)
+    getArticle();
+}
 
 LatestNews();
 
