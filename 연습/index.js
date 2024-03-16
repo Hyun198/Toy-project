35e4eeb97b7b1355dc7ff3090026924cdaa458cb
Hyun <hwn123h@naver.com>
2024-03-16 22:37:37 +0900

    reduce메서드

--- 연습/index.js
+++ 연습/index.js
@@ -1,53 +1,11 @@
-let names = [
-    "Steven Paul jobs",
-    "Bill Gates",
-    "Mark Elliot Zuckerberg",
-    "Elon Musk",
-    "Jeff Bezos",
-    "Warren Edward Buffett",
-    "Larry Page",
-    "Larry Ellison",
-    "Tim Cook",
-    "Lioyd Blankfein"
-];
+// reduce 메서드
+//arr.reduce(callback(accumulator, currentValue, index, array), initialValue);
+// 배열.reduce(callback(누적값, 현재값, 인덱스, 요소), 초기값);
 
+const arr = [1, 2, 3, 4, 5]
 
-/* names.map((item) => {
-    console.log(item.toUpperCase())
-}) */
+arr.reduce(function (acc, cur, idx) {
+    console.log("acc :" + acc, "cur :" + cur, "idx: " + idx);
+    return acc + cur;
+}, 0);
 
-/* let initial = names.map((item) => {
-    let splitName = item.split(" ");
-    let initial = "";
-    splitName.forEach((nameItem) => (initial += nameItem[0]));
-
-    return initial;
-    splitName.forEach((nameItem) => (initial += nameItem[0]));
-}) */
-
-
-/* names.filter((item) => {
-    if (item.includes("a")) {
-        console.log(item);
-    }
-}); */
-
-
-
-/* console.log(names.some((item) => { item.length > 20 })) */
-
-
-/* let double = names.filter((item) => {
-    let splitName = item.split("");
-    return splitName.some((letter, index) => letter == splitName[index + 1]);
-})
-
-console.log(double); */
-
-console.log(1)
-
-setTimeout(() => {
-    console.log(2)
-}, 5000);
-
-console.log(3);
