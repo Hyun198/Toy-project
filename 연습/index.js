65491be4ab110cf0f0a6c886ad4cfb7ed8d86dd6
Hyun <hwn123h@naver.com>
2024-03-25 11:00:30 +0900

    원형큐

--- 연습/index.js
+++ 연습/index.js
@@ -1,11 +1,75 @@
-// reduce 메서드
-//arr.reduce(callback(accumulator, currentValue, index, array), initialValue);
-// 배열.reduce(callback(누적값, 현재값, 인덱스, 요소), 초기값);
+class CircularQueue {
+    constructor(capacity) {
+        this.items = new Array(capacity)
+        this.capacity = capacity
+        this.currentLength = 0;
+        this.rear = -1;
+        this.front = -1;
+    }
 
-const arr = [1, 2, 3, 4, 5]
 
-arr.reduce(function (acc, cur, idx) {
-    console.log("acc :" + acc, "cur :" + cur, "idx: " + idx);
-    return acc + cur;
-}, 0);
+    isFull() {
+        return this.currentLength === this.capacity
+    }
+    isEmpty() {
+        return this.currentLength === 0;
+    }
 
+    enqueue(element) {
+        if (!this.isFull()) {
+            this.rear = (this.rear + 1) % this.capacity;
+            this.items[this.rear] = element
+            this.currentLength += 1;
+            if (this.front === -1) {
+                this.front = this.rear
+            }
+        }
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            return null
+        }
+        const item = this.items[this.front]
+        this.items[this.front] = null
+        this.front = (this.front + 1) % this.capacity
+        this.currentLength -= 1;
+        if (this.isEmpty()) {
+            this.front = -1
+            this.rear = -1
+        }
+        return item
+    }
+
+    peak() {
+        if (!this.isEmpty()) {
+            return this.items[this.front]
+        }
+        return null
+    }
+
+    print() {
+        if (this.isEmpty()) {
+            console.log('Queue is empty')
+        } else {
+            let i
+            let str = ''
+            for (i = this.front; i !== this.rear; i = (i + 1) % this.capacity) {
+                str += this.items[i] + " "
+            }
+            str += this.items[i]
+            console.log(str);
+        }
+    }
+}
+
+const queue = new CircularQueue(5);
+
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+queue.enqueue(40)
+queue.enqueue(50)
+
+console.log(queue.isFull())
+queue.print();
