b1fc67050e4ac30609c6f0b077aa412516a61b17
Hyun <hwn123h@naver.com>
2024-03-26 14:42:38 +0900

    list

--- 연습/index.js
+++ 연습/index.js
@@ -88,12 +88,69 @@ class LinkedList {
             this.head = node
         } else {
             node.next = this.head
+            this.head = node
         }
         this.size++
+    }
+
+    append(value) {
+        const node = new Node(value)
+        if (this.isEmpty()) {
+            this.head = node
+        } else {
+            let prev = this.head
+            while (prev.next) {
+                prev = prev.next
+            }
+            prev.next = node
+        }
+        this.size++
+    }
+
+    insert(value, index) {
+        if (index < 0 || index > this.size) {
+            return
+        }
+        if (index === 0) {
+            this.prepend(value)
+        } else {
+            const node = new Node(value)
+            let prev = this.head
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next
+            }
+            node.next = prev.next
+            prev.next = node
+            this.size++
+        }
+
+    }
+
+    print() {
+        if (this.isEmpty()) {
+            console.log('List is empty');
+        } else {
+            let curr = this.head
+            let listValues = ''
+            while (curr) {
+                listValues += `${curr.value} `
+                curr = curr.next
+            }
+            console.log(listValues)
+        }
     }
 }
 
 const list = new LinkedList()
-list.prepend(10)
-list.prepend(20)
-list.prepend(30)
+list.print()
+list.insert(10, 0)
+list.print()
+
+list.insert(20, 0)
+list.print()
+list.insert(30, 1)
+list.print()
+
+list.insert(40, 2)
+list.print()
+console.log(list.getSize())
